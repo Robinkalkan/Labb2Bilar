19247d0e530e3c5f787c466864a58a69fce44dd4
Robinkalkan <Robin.kalkan@chasacademy.se>
2023-12-14 10:23:10 +0100

    Add project files.

---
+++ Cars.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2Bilar
+{
+    class Cars
+    {
+        public string Name { get; set; }
+        public int Distance { get; set; }
+        public int Speed { get; protected set; }
+
+        public Cars(string name, int speed)
+        {
+            Name = name;
+            Speed = speed;
+            Distance = 0;
+        }
+      
+    public virtual void Race()
+        {
+            Console.WriteLine($"{Name} starts the race!");
+            Console.WriteLine("=======================");
+            while (Distance < 10)
+            {
+                Move();
+                CheckForEvent();
+                Thread.Sleep(1000);
+
+               
+                if (Distance % 3 == 0) 
+                {
+                    Console.WriteLine($"Checking for events for {Name}...");
+                    CheckForEvent();
+                    
+                }
+            }
+
+            Console.WriteLine($"{Name} finished the race!");
+        }
+
+        public void Move()
+        {
+            Distance += Speed;
+            Console.WriteLine($"{Name} is at {Distance} km.");
+        }
+
+        public void CheckForEvent()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 51);
+
+            if (randomNumber == 1)
+            {
+                Console.WriteLine($"{Name} has run out of gas. Needs to refuel and stops for 30 seconds.");
+                Thread.Sleep(30000); 
+            }
+            else if (randomNumber <= 3) 
+            {
+                Console.WriteLine($"{Name} has a flat tire. Needs to change the tire and stops for 20 seconds.");
+                Thread.Sleep(20000);
+            }
+            else if (randomNumber <= 8) 
+            {
+                Console.WriteLine($"{Name} has a bird on the windshield. Needs to clean it and stops for 10 seconds.");
+                Thread.Sleep(10000);
+            }
+            else if (randomNumber <= 18)
+            {
+                Console.WriteLine($"{Name}'s engine has a problem. Speed reduced by 1 km/h.");
+                Speed = Math.Max(Speed - 1, 0); 
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Linq;
+
+
+namespace Labb2Bilar;
+class Program
+{
+    static void Main()
+    {
+        List<Cars> cars = new List<Cars>
+        {
+            new Cars("TheDestroyer", 120),
+            new Cars("TheBolt", 120),
+            new Cars("WingedBeast", 120),
+            new Cars("SpeedOfLight", 120),
+        };
+
+        List<Thread> threads = new List<Thread>();
+
+      
+        foreach (var car in cars)
+        {
+            Thread thread = new Thread(() => RaceWithEvents(car));
+            threads.Add(thread);
+            thread.Start();
+         
+
+        }
+
+        
+        foreach (var thread in threads)
+        {
+            thread.Join();
+            
+        }
+        
+        Console.WriteLine("\t ~~~~~~~~~~ Race finished! ~~~~~~~~~~~~");
+       
+
+        
+        Console.WriteLine("Press Enter to get the race status:");
+        Console.ReadLine();
+        PrintRaceStatus(cars);
+    }
+
+    static void RaceWithEvents(Cars car)
+    {
+        Console.WriteLine($"{car.Name} starts the race!");
+
+        while (car.Distance < 10)
+        {
+            car.Move();
+            car.CheckForEvent();
+            Thread.Sleep(1000); 
+            if (car.Distance % 3 == 0) 
+            {
+                Console.WriteLine($"Checking for events for {car.Name}...");
+                car.CheckForEvent();
+            }
+        }
+
+        Console.WriteLine($"{car.Name} finished the race!");
+    }
+
+    static void PrintRaceStatus(List<Cars> cars)
+    {
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Name} - Distance: {car.Distance} km - Speed: {car.Speed} km/h");
+        }
+    }
+}
